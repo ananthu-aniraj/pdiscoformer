e24bcc0d5ca50803861502d21a6ab01be2b34817
Ananthu Aniraj <ananthu.aniraj@inria.fr>
2024-09-26 15:34:34 +0200

    refactor hubconf

--- hubconf.py
+++ hubconf.py
@@ -3,6 +3,8 @@ from torch_hub import (pdiscoformer_cub_k_8, pdiscoformer_cub_k_4, pdiscoformer_
                        pdiscoformer_pimagenet_k_25, pdiscoformer_pimagenet_k_50, pdiscoformer_pimagenet_k_8,
                        pdiscoformer_pimagenet_seg_k_8, pdiscoformer_pimagenet_seg_k_16, pdiscoformer_pimagenet_seg_k_25,
                        pdiscoformer_pimagenet_seg_k_41, pdiscoformer_pimagenet_seg_k_50,
-                       pdiscoformer_nabirds_k_4, pdiscoformer_nabirds_k_8, pdiscoformer_nabirds_k_11)
+                       pdiscoformer_nabirds_k_4, pdiscoformer_nabirds_k_8, pdiscoformer_nabirds_k_11,
+                       pdisconet_vit_nabirds_k_4, pdisconet_vit_nabirds_k_8, pdisconet_vit_nabirds_k_11,
+                       pdisconet_resnet_nabirds_k_4, pdisconet_resnet_nabirds_k_8, pdisconet_resnet_nabirds_k_11)
 
 dependencies = ['torch', 'torchvision', 'timm']
